f8572b0c15ce461b390642936274de7f68023542
Chris Vancoillie <chris@lightspeedwp.agency>
2026-07-29 12:03:03 +0200

    Use valid PHP identifiers so the template parses

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Sets up theme supports.
  */
-function {{TEXT_DOMAIN}}_setup() {
+function ls_starter_setup() {
 	// Make theme available for translation.
 	load_theme_textdomain( '{{TEXT_DOMAIN}}', get_template_directory() . '/languages' );
 
@@ -25,7 +25,7 @@ function {{TEXT_DOMAIN}}_setup() {
 	// Enqueue editor styles.
 	add_editor_style( 'style.css' );
 }
-add_action( 'after_setup_theme', '{{TEXT_DOMAIN}}_setup' );
+add_action( 'after_setup_theme', 'ls_starter_setup' );
 
 /**
  * Enqueues front-end assets.
@@ -33,7 +33,7 @@ add_action( 'after_setup_theme', '{{TEXT_DOMAIN}}_setup' );
  * Add CSS and JS files to assets/css/ and assets/js/ and uncomment
  * the relevant lines below once those files exist.
  */
-function {{TEXT_DOMAIN}}_enqueue_assets() {
+function ls_starter_enqueue_assets() {
 	// Main stylesheet (the theme header stylesheet is loaded automatically).
 	// Uncomment when assets/css/main.css exists:
 	// wp_enqueue_style(
@@ -52,4 +52,4 @@ function {{TEXT_DOMAIN}}_enqueue_assets() {
 	// 	true
 	// );
 }
-add_action( 'wp_enqueue_scripts', '{{TEXT_DOMAIN}}_enqueue_assets' );
+add_action( 'wp_enqueue_scripts', 'ls_starter_enqueue_assets' );
